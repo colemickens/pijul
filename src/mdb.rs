5f1a73f2fbab23262c8b9973a76e43afdc104c86
pe <pe@patoline.org>
2015-10-28 13:50:27 +0200

    Cursor operations
    Ignore-this: 6c78064fada7a029e2d35cb5510263eb
    
    darcs-hash:ba433d9a2e293ae65e5c0723f00127b4a6ccde4c

--- src/mdb.rs
+++ src/mdb.rs
@@ -8,11 +8,16 @@ use self::libc::types::os::arch::posix88::mode_t;
 use std::fmt;
 
 #[allow(missing_copy_implementations)]
-pub enum MDB_env {}
-pub enum MDB_txn {}
+enum MDB_env {}
+enum MDB_txn {}
+enum MDB_cursor {}
+
+pub type Env=MDB_env;
+pub type Txn=MDB_txn;
+pub type Cursor=MDB_cursor;
 
 #[repr(C)]
-pub enum MDB_cursor_op {
+enum MDB_cursor_op {
     MDB_FIRST,
     MDB_FIRST_DUP,
     MDB_GET_BOTH,
@@ -45,10 +50,11 @@ pub const MDB_NOTFOUND: c_int = -30798;
 
 
 #[repr(C)]
-pub struct MDB_val {
+struct MDB_val {
     pub mv_size:size_t,
     pub mv_data: *const u8
 }
+pub type Val=MDB_val;
 
 impl fmt::Display for MDB_val {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -80,4 +86,10 @@ extern "C" {
     pub fn mdb_dbi_open(txn: *mut MDB_txn, name: *const c_char, flags:c_uint, dbi:*mut c_uint)->c_int;
     pub fn mdb_get(txn: *mut MDB_txn, dbi:c_uint, key: *mut MDB_val, val:*mut MDB_val)->c_int;
     pub fn mdb_put(txn: *mut MDB_txn, dbi:c_uint, key: *mut MDB_val, val:*mut MDB_val,flags:c_uint)->c_int;
+    pub fn mdb_cursor_get(cursor: *mut MDB_cursor, key: *mut MDB_val, val:*mut MDB_val,flags:c_uint)->c_int;
+    pub fn mdb_cursor_put(cursor: *mut MDB_cursor, key: *mut MDB_val, val:*mut MDB_val,flags:c_uint)->c_int;
+    pub fn mdb_cursor_del(cursor: *mut MDB_cursor, flags:c_uint)->c_int;
+
+    pub fn mdb_cursor_open(txn: *mut MDB_txn, dbi:c_uint, cursor:*mut *mut MDB_cursor)->c_int;
+    pub fn mdb_cursor_close(cursor: *mut MDB_cursor);
 }
--- src/repository.rs
+++ src/repository.rs
@@ -8,13 +8,13 @@ use std::slice;
 use std::fmt;
 use std::str;
 use std;
-use mdb::{MDB_env,MDB_txn,mdb_env_create,mdb_reader_check,mdb_env_set_maxdbs,mdb_env_set_mapsize,mdb_env_open,mdb_txn_begin,mdb_txn_commit,mdb_txn_abort,mdb_env_close,mdb_dbi_open,MDB_CREATE,MDB_DUPSORT,MDB_val,MDB_NOTFOUND};
+use mdb::{Env,Txn,Val,Cursor,mdb_env_create,mdb_reader_check,mdb_env_set_maxdbs,mdb_env_set_mapsize,mdb_env_open,mdb_txn_begin,mdb_txn_commit,mdb_txn_abort,mdb_env_close,mdb_dbi_open,mdb_cursor_open,mdb_cursor_close,mdb_cursor_get,mdb_cursor_put,mdb_cursor_del,MDB_CREATE,MDB_DUPSORT,MDB_NOTFOUND};
 use mdb;
 
 pub fn with_repository<T,F>(path:&str,f: F)->Result<T,c_int>
-    where F:Fn(*mut MDB_env,*mut MDB_txn)->Result<T,c_int> {
+    where F:Fn(*mut Env,*mut Txn)->Result<T,c_int> {
         unsafe {
-            let env: *mut MDB_env = ptr::null_mut();
+            let env: *mut Env = ptr::null_mut();
             mdb_env_create(std::mem::transmute(&env));
             let mut dead:c_int=0;
             let e=mdb_reader_check(env,&mut dead);
@@ -25,7 +25,7 @@ pub fn with_repository<T,F>(path:&str,f: F)->Result<T,c_int>
             if e !=0 { println!("mdb_env_set_mapsize");return Err(e) };
             let e=mdb_env_open(env,path.as_ptr() as *const c_char,0,0o755);
             if e !=0 { println!("mdb_env_open");return Err(e) };
-            let txn: *mut MDB_txn = ptr::null_mut();
+            let txn: *mut Txn = ptr::null_mut();
             let e=mdb_txn_begin(env,ptr::null_mut(),0,std::mem::transmute(&txn));
             if e!=0 { println!("mdb_txn_begin");mdb_txn_abort(txn);return Err(e) };
             let x=
@@ -44,8 +44,20 @@ pub fn with_repository<T,F>(path:&str,f: F)->Result<T,c_int>
         }
     }
 
+fn with_cursor<T,F>(txn:*mut Txn,dbi:c_uint,f:F)->Result<T,c_int>
+    where F:Fn(*mut Cursor)->Result<T,c_int> {
+        unsafe {
+            let mut cursor:*mut Cursor=ptr::null_mut();
+            let ok=mdb_cursor_open(txn,dbi,std::mem::transmute(&txn));
+            if ok!=0 { return Err(ok) };
+            let x=f(cursor);
+            mdb_cursor_close(cursor);
+            x
+        }
+    }
+
 pub struct Repository {
-    pub t:*mut MDB_txn,
+    pub t:*mut mdb::Txn,
     pub nodes: Result<c_uint,c_int>,
     pub contents: Result<c_uint,c_int>,
     pub revdep: Result<c_uint,c_int>,
@@ -59,8 +71,52 @@ pub struct Repository {
     pub current_branch: Vec<u8>
 }
 
+pub fn open_base(t:*mut Txn,base:&mut Result<c_uint,c_int>, name:&str, flags:c_uint)->Result<c_uint,c_int>{
+    match *base {
+        Ok(n)=>Ok(n),
+        Err(_)=>{
+            let mut x=0;
+            let result=unsafe {mdb_dbi_open(t,name.as_ptr() as *const c_char,flags,&mut x)};
+            if result==0 {
+                *base=Ok(x);
+                *base
+            } else {
+                Err(result)
+            }
+        }
+    }
+}
 
-pub fn open_repository(txn:*mut MDB_txn)->Result<Repository,c_int> {
+fn mdb_put(t:*mut Txn,base:c_uint,key:&mut Val,value:&mut Val,flag:c_uint)->Result<(),c_int>{
+    unsafe {
+        let ret=mdb::mdb_put(t,base,key as *mut Val, value as *mut Val,flag);
+        if ret==0 {Ok(())} else
+            {Err(ret)}
+    }
+}
+fn mdb_get(t:*mut Txn,base:c_uint,key:&mut Val,value:&mut Val)->Result<bool,c_int>{
+    unsafe {
+        let ret=mdb::mdb_get(t,base,key as *mut Val, value as *mut Val);
+        if ret==0 {Ok(true)} else
+            if ret==MDB_NOTFOUND {Ok(false)} else {Err(ret)}
+    }
+}
+pub fn from_val(v:Val)->Vec<u8>{unsafe {slice::from_raw_parts(v.mv_data,v.mv_size as usize).to_vec()}}
+
+impl Repository {
+    pub fn dbi_nodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.nodes,"nodes",MDB_CREATE|MDB_DUPSORT) }
+    pub fn dbi_contents(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.contents,"contents",MDB_CREATE) }
+    pub fn dbi_revdep(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.contents,"revdep",MDB_CREATE|MDB_DUPSORT) }
+    pub fn dbi_internalhashes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.internalhashes,"internal",MDB_CREATE) }
+    pub fn dbi_externalhashes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.externalhashes,"external",MDB_CREATE) }
+    pub fn dbi_branches(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.branches,"branches",MDB_CREATE|MDB_DUPSORT) }
+    pub fn dbi_tree(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.tree,"tree",MDB_CREATE|MDB_DUPSORT) }
+    pub fn dbi_revtree(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.revtree,"revtree",MDB_CREATE) }
+    pub fn dbi_inodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.inodes,"inodes",MDB_CREATE) }
+    pub fn dbi_revinodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.revinodes,"revinodes",MDB_CREATE) }
+}
+
+pub fn open_repository(txn:*mut mdb::Txn)->Result<Repository,c_int> {
     let mut rep=Repository {
         t:txn,
         nodes: Err(0),
@@ -77,8 +133,8 @@ pub fn open_repository(txn:*mut MDB_txn)->Result<Repository,c_int> {
     };
     match rep.dbi_branches() {
         Ok(dbi)=>{
-            let mut k=MDB_val { mv_size:1 as size_t, mv_data:"\0".as_ptr() };
-            let mut v=MDB_val { mv_size:0 as size_t, mv_data:ptr::null_mut() };
+            let mut k=Val { mv_size:1 as size_t, mv_data:"\0".as_ptr() };
+            let mut v=Val { mv_size:0 as size_t, mv_data:ptr::null_mut() };
             match mdb_get(txn,dbi,&mut k,&mut v) {
                 Ok(e) => {
                     if e {rep.current_branch=from_val(v)};
@@ -91,49 +147,9 @@ pub fn open_repository(txn:*mut MDB_txn)->Result<Repository,c_int> {
     }
 }
 
-
-pub fn open_base(t:*mut MDB_txn,base:&mut Result<c_uint,c_int>, name:&str, flags:c_uint)->Result<c_uint,c_int>{
-    match *base {
-        Ok(n)=>Ok(n),
-        Err(_)=>{
-            let mut x=0;
-            let result=unsafe {mdb_dbi_open(t,name.as_ptr() as *const c_char,flags,&mut x)};
-            if result==0 {
-                *base=Ok(x);
-                *base
-            } else {
-                Err(result)
-            }
-        }
-    }
+fn add_inode(txn:*mut mdb::Txn,repo:&mut Repository,inode0:Vec<u8>)->Result<(),c_int>{
+    let dbi_nodes=try!(repo.dbi_nodes());
+    with_cursor(txn,dbi_nodes,|cursor| {
+        Ok(())
+    })
 }
-
-fn mdb_put(t:*mut MDB_txn,base:c_uint,key:&mut MDB_val,value:&mut MDB_val,flag:c_uint)->Result<(),c_int>{
-    unsafe {
-        let ret=mdb::mdb_put(t,base,key as *mut MDB_val, value as *mut MDB_val,flag);
-        if ret==0 {Ok(())} else
-            {Err(ret)}
-    }
-}
-fn mdb_get(t:*mut MDB_txn,base:c_uint,key:&mut MDB_val,value:&mut MDB_val)->Result<bool,c_int>{
-    unsafe {
-        let ret=mdb::mdb_get(t,base,key as *mut MDB_val, value as *mut MDB_val);
-        if ret==0 {Ok(true)} else
-            if ret==MDB_NOTFOUND {Ok(false)} else {Err(ret)}
-    }
-}
-
-impl Repository {
-    pub fn dbi_nodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.nodes,"nodes",MDB_CREATE|MDB_DUPSORT) }
-    pub fn dbi_contents(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.contents,"contents",MDB_CREATE) }
-    pub fn dbi_revdep(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.contents,"revdep",MDB_CREATE|MDB_DUPSORT) }
-    pub fn dbi_internalhashes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.internalhashes,"internal",MDB_CREATE) }
-    pub fn dbi_externalhashes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.externalhashes,"external",MDB_CREATE) }
-    pub fn dbi_branches(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.branches,"branches",MDB_CREATE|MDB_DUPSORT) }
-    pub fn dbi_tree(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.tree,"tree",MDB_CREATE|MDB_DUPSORT) }
-    pub fn dbi_revtree(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.revtree,"revtree",MDB_CREATE) }
-    pub fn dbi_inodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.inodes,"inodes",MDB_CREATE) }
-    pub fn dbi_revinodes(&mut self)->Result<c_uint,c_int> { open_base(self.t,&mut self.revinodes,"revinodes",MDB_CREATE) }
-}
-
-pub fn from_val(v:MDB_val)->Vec<u8>{unsafe {slice::from_raw_parts(v.mv_data,v.mv_size as usize).to_vec()}}
