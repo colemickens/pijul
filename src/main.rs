254cbbea4468e90c30102ce6b698317c2dc16097
florent.becker <florent.becker@ens-lyon.org>
2015-10-29 00:48:30 +0200

    Use clap for command line arguments
    Ignore-this: 71614a8dd6b6e0d73b3b2431cd9d43b
    
    darcs-hash:7d1e5c74c9e193db3020c61b1a64d64d605f193e

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 extern crate libc;
-#[macro_use] extern crate pijul;
+extern crate pijul;
 use pijul::repository::*;
 
 fn main() {
+    let args = clap_app!(
+        pijul =>
+            (version: "0.1.0")
+            (author: "Pierre-Étienne Meunier and Florent Becker")
+            (about: "Version Control: performant, distributed, easy to use; pick any three")
+            (@arg REPOSITORY: "Location of the repository")
+            )
+        .get_matches();
+
+    let repository = args.value_of("REPOSITORY").unwrap_or("/tmp/test");
+
     let x=
-        with_repository!("/tmp/test\0",env,txn,{
+        with_repository("/tmp/test\0",|_,txn| {
             let rep=open_repository(txn);
             rep
         });
