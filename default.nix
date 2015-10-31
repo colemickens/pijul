17eced4b33b978a4d20b2a63908373346dbcdf30
florent.becker <florent.becker@ens-lyon.org>
2015-10-31 17:14:50 +0200

    Add a default.nix
    Ignore-this: 3d34c0da6ac99864ff16f52fd4cc90a7
    
    darcs-hash:0edb713bc5d9b5c76a7dda8bb60af2508fa25130

---
+++ default.nix
@@ -0,0 +1,11 @@
+with (import <nixpkgs> {});
+
+rustPlatform.buildRustPackage rec {
+  name = "rust-pijul-${version}";
+  version = "0.0";
+  src = ./.;
+
+  buildInputs = [ lmdb ];
+
+  depsSha256 = "0g8hh29dxsq81h2zjri67jzmbg660ja3hif776vn341zknmq1n16";
+}
