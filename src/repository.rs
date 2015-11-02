55eb5a27bf2f65c32ff74286c0333d68098e5cd7
pe <pe@patoline.org>
2015-11-02 16:24:26 +0200

    Unsafe add_inode!
    Ignore-this: 7d64185ce6ba837e86b8c863dcc59008
    
    darcs-hash:285509b26808c745e187f446e1dcec2ec00cb9db

--- src/repository.rs
+++ src/repository.rs
@@ -11,24 +11,62 @@ use std;
 use std::collections::HashMap;
 extern crate rand;
 
-const INODE_SIZE:usize=16;
-
 #[allow(missing_copy_implementations)]
 pub enum MDB_env {}
 pub enum MDB_txn {}
 pub enum MDB_cursor {}
+type MDB_dbi=c_uint;
+#[repr(C)]
+pub struct MDB_val {
+    pub mv_size:size_t,
+    pub mv_data: *const c_char
+}
+
+extern "C" {
+    fn mdb_env_create(env: *mut *mut MDB_env) -> c_int;
+    fn mdb_env_open(env: *mut MDB_env, path: *const c_char, flags: c_uint, mode: mode_t) -> c_int;
+    fn mdb_env_close(env: *mut MDB_env);
+    fn mdb_env_set_maxdbs(env: *mut MDB_env,maxdbs:c_uint)->c_int;
+    fn mdb_env_set_mapsize(env: *mut MDB_env,mapsize:size_t)->c_int;
+    fn mdb_reader_check(env:*mut MDB_env,dead:*mut c_int)->c_int;
+    fn mdb_txn_begin(env: *mut MDB_env,parent: *mut MDB_txn, flags:c_uint, txn: *mut *mut MDB_txn)->c_int;
+    fn mdb_txn_commit(txn: *mut MDB_txn)->c_int;
+    fn mdb_txn_abort(txn: *mut MDB_txn);
+    fn mdb_dbi_open(txn: *mut MDB_txn, name: *const c_char, flags:c_uint, dbi:*mut MDB_dbi)->c_int;
+    fn mdb_get(txn: *mut MDB_txn, dbi:MDB_dbi, key: *mut MDB_val, val:*mut MDB_val)->c_int;
+    fn mdb_put(txn: *mut MDB_txn, dbi:MDB_dbi, key: *mut MDB_val, val:*mut MDB_val,flags:c_uint)->c_int;
+}
+
+
+pub const MDB_REVERSEKEY: c_uint = 0x02;
+pub const MDB_DUPSORT: c_uint = 0x04;
+pub const MDB_INTEGERKEY: c_uint = 0x08;
+pub const MDB_DUPFIXED: c_uint = 0x10;
+pub const MDB_INTEGERDUP: c_uint = 0x20;
+pub const MDB_REVERSEDUP: c_uint =  0x40;
+pub const MDB_CREATE: c_uint = 0x40000;
+pub const MDB_NOTFOUND: c_int = -30798;
+
+const INODE_SIZE:usize=16;
+const MAX_DBS:usize=3;
+
+pub enum DBI {
+    NODES,
+    TREE,
+    REVTREE
+}
+
+const dbis:[(&'static str,c_uint);MAX_DBS]=[("nodes\0",MDB_CREATE|MDB_DUPSORT),
+                                            ("tree\0",MDB_CREATE),
+                                            ("revtree\0",MDB_CREATE)
+                                            ];
 
 pub struct Repository{
-    mdb_env:*mut MDB_env
+    mdb_env:*mut MDB_env,
+    mdb_txn:*mut MDB_txn,
+    dbi_:[Option<MDB_dbi>;MAX_DBS]
 }
-extern "C" {
-    pub fn mdb_env_create(env: *mut *mut MDB_env) -> c_int;
-    pub fn mdb_env_open(env: *mut MDB_env, path: *const c_char, flags: c_uint, mode: mode_t) -> c_int;
-    pub fn mdb_env_close(env: *mut MDB_env);
-    pub fn mdb_env_set_maxdbs(env: *mut MDB_env,maxdbs:c_uint)->c_int;
-    pub fn mdb_env_set_mapsize(env: *mut MDB_env,mapsize:size_t)->c_int;
-    pub fn mdb_reader_check(env:*mut MDB_env,dead:*mut c_int)->c_int;
-}
+
 impl Repository {
     pub fn new(path:&std::path::Path)->Result<Repository,c_int>{
         unsafe {
@@ -38,7 +76,7 @@ impl Repository {
             let mut dead:c_int=0;
             let e=mdb_reader_check(env,&mut dead);
             if e != 0 { println!("mdb_reader_check");return Err(e) };
-            let e=mdb_env_set_maxdbs(env,10);
+            let e=mdb_env_set_maxdbs(env,MAX_DBS as c_uint);
             if e != 0 { println!("mdb_env_set_maxdbs");return Err(e) };
             let e=mdb_env_set_mapsize(env,std::ops::Shl::shl(1,30) as size_t);
             if e !=0 { println!("mdb_env_set_mapsize");return Err(e) };
@@ -48,8 +86,14 @@ impl Repository {
                     let e=mdb_env_open(env,pp.as_ptr() as *const i8,0,0o755);
                     if e !=0 { println!("mdb_env_open");return Err(e) };
 
+                    let txn=ptr::null_mut();
+                    let e=mdb_txn_begin(env,ptr::null_mut(),0,std::mem::transmute(&txn));
+                    if e !=0 { println!("mdb_env_open");return Err(e) };
+
                     let repo=Repository{
-                        mdb_env:env
+                        mdb_env:env,
+                        mdb_txn:txn,
+                        dbi_:[None;MAX_DBS]
                     };
                     Ok(repo)
                 },
@@ -60,14 +104,81 @@ impl Repository {
             }
         }
     }
+    fn dbi(&mut self,num:DBI)->MDB_dbi{
+        let n=num as usize;
+        match self.dbi_[n] {
+            Some(dbi) => {dbi},
+            None => {
+                unsafe {
+                    let d=0;
+                    let (name,flag)=dbis[n];
+                    unsafe {mdb_dbi_open(self.mdb_txn,name.as_ptr() as *const c_char,flag,std::mem::transmute(&d))};
+                    self.dbi_[n]=Some(d);
+                    d
+                }
+            }
+        }
+    }
 }
 
 impl Drop for Repository {
     fn drop(&mut self){
-        unsafe { mdb_env_close(self.mdb_env) }
+        unsafe {
+            mdb_txn_abort(self.mdb_txn);
+            mdb_env_close(self.mdb_env)
+        }
     }
 }
 
-fn add_inode(repo:&mut Repository, inode:&Option<[u8;INODE_SIZE]>, path:&std::path::Component){
-    ()
+fn add_inode(repo:&mut Repository, inode:&Option<[c_char;INODE_SIZE]>, path:&std::path::Path)->Result<(),()>{
+    let mut buf:Vec<c_char>=Vec::with_capacity(INODE_SIZE);
+    // Init to 0
+    for i in 0..INODE_SIZE-1 {
+        buf.push(0)
+    }
+    let mut components=path.components();
+    let mut cs=components.next();
+    while cs.is_some(){
+        let s=cs.unwrap();
+        cs=components.next();
+        match s.as_os_str().to_str(){
+            Some(ss) => {
+                buf.truncate(INODE_SIZE);
+                for c in ss.as_bytes() { buf.push(*c as c_char) }
+                let mut k=MDB_val { mv_data:buf.as_ptr(), mv_size:buf.len()as size_t };
+                let mut v=MDB_val { mv_data:ptr::null_mut(), mv_size:0 };
+                let ret= unsafe { mdb_get(repo.mdb_txn,repo.dbi(DBI::TREE),&mut k,&mut v) };
+                if ret==0 {
+                    // replace buf with existing inode
+                    buf.clear();
+                    let pv:*const c_char=v.mv_data;
+                    unsafe { for c in 0..v.mv_size-1 { buf.push(*pv.offset(c as isize)) } }
+                } else {
+                    let inode = if cs.is_none() && inode.is_some() {
+                        inode.unwrap()
+                    } else {
+                        let mut inode:[c_char;INODE_SIZE]=[0;INODE_SIZE];
+                        for i in 0..INODE_SIZE-1 { inode[i]=rand::random() }
+                        inode
+                    };
+                    v.mv_data=inode.as_ptr();
+                    v.mv_size=INODE_SIZE as size_t;
+                    unsafe { mdb_put(repo.mdb_txn,repo.dbi(DBI::TREE),&mut k,&mut v,0) };
+                    unsafe { mdb_put(repo.mdb_txn,repo.dbi(DBI::REVTREE),&mut v,&mut k,0) };
+                    if cs.is_some() {
+                        k.mv_data="".as_ptr() as *const c_char;
+                        k.mv_size=0;
+                        unsafe { mdb_put(repo.mdb_txn,repo.dbi(DBI::TREE),&mut v,&mut k,0) };
+                    }
+                    // push next inode onto buf.
+                    buf.clear();
+                    for c in &inode { buf.push(*c) }
+                }
+            },
+            None => {
+                return Err(())
+            }
+        }
+    }
+    Ok(())
 }
