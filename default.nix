dfd4c96485cd8d0c1d775a6defcccefcfc8919dd
florent.becker <florent.becker@ens-lyon.org>
2015-11-20 14:19:44 +0200

    Update default.nix
    Ignore-this: 43902ff85f7807ea51a0a7bfc29e3882
    
    darcs-hash:ea90d07eec4f828f40f18480e44cdac20c9bac6e

--- default.nix
+++ default.nix
@@ -5,9 +5,8 @@ rustUnstable.buildRustPackage rec {
   version = "0.0";
   src = ./.;
 
-  buildInputs = [ lmdb ];
+  buildInputs = [ lmdb openssl libssh2 zlib pkgconfig ];
   
-  depsSha256 = "0g8hh29dxsq81h2zjri67jzmbg660ja3hif776vn341zknmq1n16";
-  
+  depsSha256 = "0g8hh29dxsq81h2zjri67jzmbg660ja3hif776vn341zknmq1n17";
   
 }
