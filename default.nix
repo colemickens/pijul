3149412e0622c9624fd7fd780eef00f65dcda5dd
florent.becker <florent.becker@ens-lyon.org>
2015-11-27 12:46:26 +0200

    stop using buildRustPackage in default.nix
    Ignore-this: 69586b2f12fa240a2fed9bc09ee64a40
    
    It is not usable for making a shell.
    
    darcs-hash:d5418b8ff73dc60f354e00e73ae0f3ec960b02ea

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
 with (import <nixpkgs> {});
 
-rustUnstable.buildRustPackage rec {
+stdenv.mkDerivation rec {
   name = "rust-pijul-${version}";
   version = "0.0";
   src = ./.;
 
-  buildInputs = [ lmdb openssl libssh2 zlib pkgconfig ];
+  buildInputs = [ rustPlatform.rustc rustPlatform.cargo lmdb openssl libssh2 zlib pkgconfig ];
   
   depsSha256 = "0g8hh29dxsq81h2zjri67jzmbg660ja3hif776vn341zknmq1n17";
   
