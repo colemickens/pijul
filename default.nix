3204b6f95108baa02e1e698eeea2d1c07c2540c5
florent.becker <florent.becker@ens-lyon.org>
2016-01-15 19:37:35 +0200

    Use libssh, not libssh2 in default.nix
    Ignore-this: c0589c2bee642078bb455a7bb4f7e6ae
    
    darcs-hash:2f4be3f59a909c9c31aed3891ec4896acf88b521

--- default.nix
+++ default.nix
@@ -5,8 +5,6 @@ stdenv.mkDerivation rec {
   version = "0.0";
   src = ./.;
 
-  buildInputs = [ rustPlatform.rustc rustPlatform.cargo lmdb openssl libssh2 zlib pkgconfig ];
-  
-  depsSha256 = "0g8hh29dxsq81h2zjri67jzmbg660ja3hif776vn341zknmq1n17";
+  buildInputs = [ rustPlatform.rustc rustPlatform.cargo lmdb openssl libssh zlib pkgconfig ];
   
 }
