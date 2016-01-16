b28e0d28e45b6085471256c8636cc62f5ebdb28d
florent.becker <florent.becker@ens-lyon.org>
2016-01-16 11:06:48 +0200

    Add a cabal file to libhspijul
    Ignore-this: 7f7ca596cd9b379d3fde02fd427b547b
    
    This is needed for cabalized programs to be able to find libhspijul
    
    darcs-hash:92457dba2b696f3640d45d89202a92d52c6a7d75

---
+++ haskell/Pijul.hs
@@ -0,0 +1,44 @@
+module Pijul( createRepository ) where
+
+import Foreign.C.Types
+import Foreign.C.String
+import Foreign.Ptr
+import Control.Exception
+import Foreign.Marshal.Alloc
+import Foreign.Storable
+import Data.Typeable
+import qualified Data.ByteString as B
+
+data CRepository
+
+foreign import ccall pijul_open_repository :: CString -> Ptr (Ptr CRepository) -> IO CInt
+foreign import ccall pijul_close_repository :: Ptr CRepository -> IO ()
+
+type Repository = Ptr CRepository
+
+data Exn = Load deriving (Show, Typeable)
+
+instance Exception Exn
+
+
+withRepository::String -> (Repository -> IO a) -> IO a
+withRepository path f =
+    withCString path $ \cpath->
+        bracket
+        (alloca $ \p->do { op<-pijul_open_repository cpath p;
+                           if op ==0 then peek p
+                           else throw Load })
+        (\p -> do { pijul_close_repository p })
+        f
+
+foreign import ccall pijul_add_file :: Ptr CRepository -> CString->CInt-> IO ()
+
+addFile::Repository->String->Bool->IO ()
+addFile rep path isDir=
+    withCString path $ \cpath->pijul_add_file rep cpath (if isDir then 1 else 0)
+
+
+
+main=
+  withRepository "/tmp/a" $ \repo->
+      addFile repo "file" False
