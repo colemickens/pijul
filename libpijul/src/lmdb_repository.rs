e56a9b12bf94dba8b6f6e39c1573f670252a8886
Florent Becker <florent.becker@ens-lyon.org>
2016-02-01 18:58:26 +0200

    Add an implementation of repository, using lmdb
    Ignore-this: f3a254188f64cf9c92d71b96ed08e4a8
    
    darcs-hash:9b7df88b8e179cad472773e819d6f52964db2675

---
+++ libpijul/src/lmdb_repository.rs
@@ -0,0 +1,44 @@
+extern crate std;
+
+use lmdb;
+use contents;
+
+pub struct LmdbRepository<'a> {
+    pub env : lmdb::Env,
+    pub txn : lmdb::Txn<'a>,
+    pub dbi_nodes : lmdb::Dbi,
+    pub dbi_revdep : lmdb::Dbi,
+    pub dbi_contents : lmdb::Dbi,
+    pub dbi_internal : lmdb::Dbi,
+    pub dbi_external : lmdb::Dbi,
+    pub dbi_branches : lmdb::Dbi,
+    pub dbi_tree : lmdb::Dbi,
+    pub dbi_revtree : lmdb::Dbi,
+    pub dbi_inodes : lmdb::Dbi,
+    pub dbi_revinodes : lmdb::Dbi
+}
+
+impl <'a>Drop for LmdbRepository<'a> {
+    fn drop(& mut self){
+        unsafe {
+            if std::thread::panicking() {
+                self.txn.unsafe_abort()
+            } else {
+                self.txn.unsafe_commit().unwrap()
+            }
+        }
+    }
+}
+
+impl <'a> LmdbRepository<'a> {
+    pub fn get_file_content<'b>(self : &'b LmdbRepository<'a>, inode: contents::Inode)
+                                -> Result<Option<&'b[u8]>, std::io::Error>
+    {
+        self.txn.get(self.dbi_tree, &(inode.inode_contents))
+    }
+
+    
+}
+
+
+    
